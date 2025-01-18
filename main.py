d25aee7bbfdb27ed04913f515256db2a2f038ed7
JoanneHing <yenxinghing@gmail.com>
2025-01-18 15:26:47 +0800

    Testing

--- main.py
+++ main.py
@@ -18,7 +18,21 @@ def start(update: Update, context: CallbackContext) -> None:
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     update.message.reply_text("Choose an option: ", reply_markup=reply_markup)
+    if update.message.text == "SoC":
+        SoC(update, context)
 
+def SoC(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.chat_id
+    drive_file_url = "https://drive.google.com/file/d/1cY6yrE8o6Io-w8ufLQgT2Nt7Um9PWkxp/view"
+    response = requests.get(drive_file_url)
+    if response.status_code == 200:
+        with open("temp.pdf", "wb") as pdf_file:
+            pdf_file.write(response.content)
+        
+        # Send the PDF to the user
+        context.bot.send_document(chat_id=chat_id, document=open("temp.pdf", "rb"))
+    else:
+        update.message.reply_text("Failed to fetch the file from Google Drive.")
 
 def notes(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Do it yourself la')
@@ -28,6 +42,7 @@ def main() -> None:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("notes", notes))
+    dispatcher.add_handler(CommandHandler("SoC", SoC))
 
     updater.start_polling()
     updater.idle()
