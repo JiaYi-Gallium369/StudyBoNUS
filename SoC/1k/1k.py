2b5319c0e6e5698525bd6ff91822b7270dcfc309
annabellekk <yuyien.khoo@gmail.com>
2025-01-18 20:01:43 +0800

    Add 1k courses files

--- SoC/1k/1k.py
+++ SoC/1k/1k.py
@@ -1,1 +1,48 @@
 # for 1k mods
+COURSE_INFO_1K = {
+    'CS1101S': {
+        'description': 'Programming Methodology',
+        'materials': {
+            'Notes': ['https://drive.google.com/file/d/1373e_2oDvZuMHOUqP59tpIJ1R-iBiNdx/view?usp=sharing'],
+            'Slides': ['https://drive.google.com/file/d/1213GLuXlTL0jPZLSpECZtVaH6rVcqwtK/view?usp=sharing', 'https://drive.google.com/file/d/12-LMHTvqDM2nKyPvP9kgWeqW6TFJ2Bhc/view?usp=sharing'],
+            'Cheatsheet': {
+                'Midterm': 'https://drive.google.com/file/d/1CUb-ufwrGKW-vg0mu06l-fS2fo4M35nf/view?usp=sharing',
+                'Final': 'https://drive.google.com/file/d/184TJtUi0N4mR8FXufQvpd-n17b0PYJSP/view?usp=sharing'
+            },
+            'Past Papers': {
+                'Midterm': {
+                    'EARLIER': 'https://drive.google.com/file/earlier_midterm_paper_id/view?usp=sharing',
+                    '18/19': ['https://drive.google.com/file/d/1J5Qk_K55QGiVh1WhUKYmjD4RiSXMlj31/view?usp=sharing', 'https://drive.google.com/file/d/1B-OIaohj0g8k-lM_rMdW_jpHcwtIMdsa/view?usp=sharing'],
+                    '19/20': ['https://drive.google.com/file/d/1wJSRqrnWPjeXqCHK4Xx-P1gq6sdEZlCC/view?usp=sharing', 'https://drive.google.com/file/d/1XkmojZ4CtUpdyLCNzYy50DIW0SePGl70/view?usp=sharing'],
+                    '20/21': ['https://drive.google.com/file/d/1Oe8swsBnReSCL3khGaNVxKKxwRP83zfz/view?usp=sharing', 'https://drive.google.com/file/d/1mAtLy3NNBCgEmuVDLUrMKbyPyuCSXJJP/view?usp=sharing']
+                },
+                'Final': {
+                    'EARLIER': 'https://drive.google.com/file/earlier_final_paper_id/view?usp=sharing',
+                    '19/20': ['https://drive.google.com/file/d/1coEzmOL4N7HCYPsWWwxjOpTPtUMGzaDI/view?usp=sharing', 'https://drive.google.com/file/d/1zNTv5aczZueOP7JCBf-GIrC21DLxlzfJ/view?usp=sharing'],
+                    '20/21': ['https://drive.google.com/file/d/1K6WHsdztStb212Tj8gkOUtJAlQJmHztL/view?usp=sharing', 'https://drive.google.com/file/d/1qrU4oGVQ1s9ZsudrGz4SQiJ8E5u1ORNI/view?usp=sharing']
+                }
+            }
+    },
+    'CS1231S': {
+        'description': 'Discrete Structures',
+        'materials': {
+            'Notes': 'https://drive.google.com/file/d/1eCDBd9u__fB-FaVfY92TwNCgf76zujvK/view?usp=sharing',
+            'Slides': 'https://drive.google.com/file/d/1yUw-Ev5NyxUxyuSupdvuyYtD82Psl2qa/view?usp=sharing',
+            'Cheatsheet': {
+                'Midterm': [],
+                'Final': []
+            },
+            'Past Papers': {
+                'Midterm': {
+                    'EARLIER': 'https://drive.google.com/file/d/1373e_2oDvZuMHOUqP59tpIJ1R-iBiNdx/view?usp=sharing',
+                    '19/20': 'https://drive.google.com/file/d/1YB4nMjdxqdIHfb5sAi3W34zKQI7ngLzd/view?usp=sharing',
+                    '20/21': 'https://drive.google.com/file/d/1g3UiIlL6owyzQaT3dW1FpJMzqSzk0E5Z/view?usp=sharing'
+                },
+                'Final': {
+                    'EARLIER': 'https://drive.google.com/file/d/1373e_2oDvZuMHOUqP59tpIJ1R-iBiNdx/view?usp=sharing',
+                    '18/19': 'https://drive.google.com/file/d/1uPw7OTsW-j5QiCCTcBpwWB8jX8avb0d_/view?usp=sharing',
+                },
+            }
+        }
+    }
+}
